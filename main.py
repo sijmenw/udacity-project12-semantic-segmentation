d2fbc2020a1c9591496c8a6e63796a7796db0961
Sijmen <sijmen.vanderwillik@jstack.eu>
2019-02-08 11:29:47 +0100

    optimize

--- main.py
+++ main.py
@@ -112,7 +112,20 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+
+    # reshape (flatten) image output
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+
+    # reshape labels
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+
+    # softmax cross entropy loss with logits
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+
+    # adam optimizer
+    train_op = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
